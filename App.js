25ce66178b8a626276592c74c78959091aa4e3a4
VjacheslavZ <slavikov.net@gmail.com>
2020-03-05 22:47:00 +0200

    add AddTodo, Todo components

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
 
 import { NavBar } from "./src/NavBar";
+import { AddTodo } from "./src/AddTodo";
+import { Todo } from "./src/Todo";
 
 export default function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (title) => {
+    setTodos(prev => [...prev, {
+      id: Date.now().toString(),
+      title,
+    }])
+  };
+
   return (
-    <View style={styles.container}>
-      <NavBar
-        title='Todo app'
-      />
+    <View>
+      <NavBar title='Todo app'/>
+
+      <View style={styles.container}>
+        <AddTodo onSubmit={addTodo} />
+
+        <View>
+          {todos.map(todo => <Todo title={todo.title} key={todo.id}/>)}
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-
+    paddingHorizontal: 30,
+    paddingVertical: 20,
   },
 });
---
+++ src/AddTodo.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet, TextInput, View, Button } from 'react-native';
+
+export const AddTodo = ({ onSubmit }) => {
+  const pressHandler = () => {
+    onSubmit('Test todo')
+  };
+
+  return (
+    <View style={styles.block}>
+      <TextInput style={styles.input}/>
+      <Button title="Add new" onPress={pressHandler}/>
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({
+  block: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 15,
+  },
+  input: {
+    width: '70%',
+    borderStyle: 'solid',
+    borderBottomWidth: 2,
+    borderBottomColor: '#3949ab',
+    padding: 10
+  }
+});
