727b6b68f05c58245303d321a804a10f956e55f1
VjacheslavZ <slavikov.net@gmail.com>
2020-03-07 14:25:44 +0200

    Implemented EditModal component,

---
+++ src/components/EditModal.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { View, StyleSheet, TextInput, Modal, Button } from 'react-native';
+import {THEME} from "../theme";
+
+export const EditModal = ({ visible, onCancel }) => {
+  return (
+    <Modal
+      visible={visible}
+      animationType="slide"
+      transparent={false}
+    >
+      <View style={styles.wrap}>
+        <TextInput
+          style={styles.input}
+          placeholder='Enter name'
+          autoCapitaleze={false}
+          autoCorrect={false}
+          maxLength={64}
+        />
+
+        <View style={styles.buttons}>
+          <Button
+            title='Cancel'
+            onPress={() => onCancel()}
+            color={THEME.DANGER_COLOR}
+          />
+          <Button title='Save' />
+        </View>
+      </View>
+    </Modal>
+  )
+};
+
+const styles = StyleSheet.create({
+  wrap: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    padding: 10,
+    borderBottomColor: THEME.MAIN_COLOR,
+    borderBottomWidth: 2,
+    width: '80%'
+  },
+  buttons: {
+    width: '100%',
+    marginTop: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  }
+});
--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -1,15 +1,27 @@
-import React  from 'react';
+import React, { useState }  from 'react';
 import { StyleSheet, View, Text, Button } from 'react-native';
 
 import { AppCard } from "../components/ui/AppCard";
+import { EditModal } from "../components/EditModal";
+
 import { THEME } from "../theme";
 
 export const TodoScreen = ({ goBack, todo, onRemove }) => {
+  const [ modal, setModal ] = useState(false);
+
   return (
     <View>
+      <EditModal
+        visible={modal}
+        onCancel={() => setModal(false )}
+      />
+
       <AppCard style={styles.card}>
         <Text style={styles.title}>{todo.title}</Text>
-        <Button title='Edit'/>
+        <Button
+          title='Edit'
+          onPress={() => setModal(true)}
+        />
       </AppCard>
 
       <View style={styles.buttons}>
