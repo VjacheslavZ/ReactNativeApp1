b71ffab471d09e043456df0701a0a08e027090d7
VjacheslavZ <slavikov.net@gmail.com>
2020-03-07 12:20:55 +0200

    add external styles, add AppCard component

--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -1,11 +1,17 @@
 import React  from 'react';
 import { StyleSheet, View, Text, Button } from 'react-native';
+
+import { AppCard } from "../components/ui/AppCard";
 import { THEME } from "../theme";
 
 export const TodoScreen = ({ goBack, todo }) => {
   return (
     <View>
-      <Text>{todo.title}</Text>
+      <AppCard style={styles.card}>
+        <Text style={styles.title}>{todo.title}</Text>
+        <Button title='Edit'/>
+      </AppCard>
+
       <View style={styles.buttons}>
         <View style={styles.button}>
           <Button
@@ -33,5 +39,12 @@ const styles = StyleSheet.create({
   },
   button: {
     width: '40%',
+  },
+  title: {
+    fontSize: 20,
+  },
+  card: {
+    marginBottom: 20,
+    padding: 15
   }
 });
